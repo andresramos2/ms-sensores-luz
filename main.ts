558c7e2208465aa5169463a9cc22ace333a2a146
andresramos2 <134411262+andresramos2@users.noreply.github.com>
2023-05-23 18:54:37 +0200

    Actualizar pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,10 +1,11 @@
 basic.forever(function () {
-    basic.showNumber(input.lightLevel())
-    if (input.lightLevel() > 5) {
+    if (input.soundLevel() > 60) {
         motor.MotorRun(motor.Motors.M1, motor.Dir.CW, 183)
+        motor.servo(motor.Servos.S8, 0)
         pins.digitalWritePin(DigitalPin.P0, 1)
     } else {
         motor.MotorRun(motor.Motors.M1, motor.Dir.CW, 0)
-        pins.digitalWritePin(DigitalPin.P0, 1)
+        motor.servo(motor.Servos.S8, 106)
+        pins.digitalWritePin(DigitalPin.P0, 0)
     }
 })
